b98d616f5fb622e74b6a0f0858b6b0ac4c4acec4
Harsh Upadhyay <upadhyayharsh435@gmail.com>
2026-02-02 23:12:40 +0530

    feat: enable CORS middleware in app.js

--- Day9/Backend/src/app.js
+++ Day9/Backend/src/app.js
@@ -1,25 +1,42 @@
-const express=require('express')
-const app=express();
-const employeeModel=require('./models/employee.model.js') 
+const express = require('express')
+const app = express()
+const employeeModel = require('./models/employee.model.js')
+const cors = require('cors')
 
-app.use(express.json());
+app.use(express.json())
+app.use(cors())
 
- 
-
-app.post('/api/employee',(req,res)=>{
-   employeeModel.create(req.body).then(
-    res.status(201).json(
-       { msg:"employee created"}
-    )
-   )
-})
-app.get('/api/employee',async (req,res)=>{
- const data=await employeeModel.find()
- res.status(200).json({data:data})
+app.post('/api/employee', async (req, res) => {
+  const data = await employeeModel.create(req.body)
+  res.status(201).json({
+    msg: "employee created",
+    data
+  })
 })
 
+app.get('/api/employee', async (req, res) => {
+  const data = await employeeModel.find()
+  res.status(200).json({ data })
+})
 
+app.patch('/api/employee/:id', async (req, res) => {
+  const data = await employeeModel.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  )
 
+  res.status(200).json({
+    msg: "employee updated",
+    data
+  })
+})
 
+app.delete('/api/employee/:id', async (req, res) => {
+  await employeeModel.findByIdAndDelete(req.params.id)
+  res.status(200).json({
+    msg: "employee deleted"
+  })
+})
 
-module.exports =app;
+module.exports = app
