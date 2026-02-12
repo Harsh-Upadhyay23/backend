28b24090d2ff4e19640e7ed37bd4cd131e1fad2d
Harsh Upadhyay <upadhyayharsh435@gmail.com>
2026-02-12 14:39:07 +0530

    feat:bcrypt added

--- Day14/src/routes/auth.routes.js
+++ Day14/src/routes/auth.routes.js
@@ -1,81 +1,10 @@
 const express=require('express');
-const userModel=require("../model/user.model")
-const crypto=require('crypto')
-const jwt=require('jsonwebtoken')
+const authController = require("../controllers/auth.controller")
 
 const authRouter=express.Router();
 
-authRouter.post('/register',async(req,res)=>{
-  const {email,username,password,bio,profileEmage}=req.body;
-   const isUserExist =await userModel.findOne({$or:[{username},{email}]})
+authRouter.post('/register',authController.registerController)
 
-   if(isUserExist){
-      return res.status(409).json({
-         message: "user already exists"+((isUserExist.email===email)?" with this email":" with this username")
-      })
-   }
-   
-   const hash=crypto.createHash('sha256').update(password).digest('hex')
-
-  const user=await userModel.create({
-      email,username,bio,profileEmage,password :hash
-   })
-
-  
-
-   const token=jwt.sign({
-      id : user._id
-   },process.env.JWT_SECRET,{expiresIn:'1d'})
-
-   res.cookie('token',token)
-   res.status(201).json({
-      message:"user created",
-      user
-   })
-})
-
-authRouter.post('/login',async(req,res)=>{
-
- const { email, password, username } = req.body;
-
-const user = await userModel.findOne({
-  $or: [
-    { email: email },
-    { username: username }
-  ]
-});
-
-if(!user){
-   return res.status(404).json({
-      message:"user not found"
-   })
-}
-
-const hash=crypto.createHash('sha256').update(password).digest('hex')
-
-const isPasswordValid= hash==user.password
-
-if(!isPasswordValid){
-   return res.status(401).json({
-      message:"password incorrect"
-   })
-}
- const token=jwt.sign({
-   user:user._id
- },process.env.JWT_SECRET,{expiresIn:"1d"})
-
- res.cookie('token',token)
- res.status(201).json({
-   message:"user logged in",
-   user:{
-      username:user.name,
-      email:user.email,
-      bio:user.bio,
-      profileImage:user.profileImage
-   }
- 
- })
-
-})
+authRouter.post('/login',authController.loginController)
 
 module.exports=authRouter;
