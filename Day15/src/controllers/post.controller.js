f67e15631c44853c461567b5ae82f8def954e2d4
Harsh Upadhyay <upadhyayharsh435@gmail.com>
2026-02-17 23:01:40 +0530

    feat: implement createPostController with ImageKit upload integration

--- Day15/src/controllers/post.controller.js
+++ Day15/src/controllers/post.controller.js
@@ -1,9 +1,32 @@
 const postModel=require('../model/post.model');
+const ImageKit = require("@imagekit/nodejs")
+const { toFile } = require("@imagekit/nodejs")
+const jwt = require("jsonwebtoken")
 
-async function createPostController(req,res){
 
-    console.log(req.body);
+const imagekit = new ImageKit({
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY
+})
 
+async function createPostController(req, res) {
+
+
+    const file = await imagekit.files.upload({
+        file: await toFile(Buffer.from(req.file.buffer), 'file'),
+        fileName: "Test",
+        folder: "cohort-2-insta-clone-posts"
+    })
+
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: req.user.id
+    })
+
+    res.status(201).json({
+        message: "Post created successfully.",
+        post
+    })
 }
 
 module.exports={createPostController}
