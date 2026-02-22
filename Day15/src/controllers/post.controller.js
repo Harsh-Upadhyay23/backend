8224e8e4c6e426fb7b5e1530298b764c1479050a
Harsh Upadhyay <upadhyayharsh435@gmail.com>
2026-02-22 23:03:43 +0530

    feat(post): add create post controller with image upload

--- Day15/src/controllers/post.controller.js
+++ Day15/src/controllers/post.controller.js
@@ -1,10 +1,46 @@
-const postModel=require('../model/post.model');
+// const postModel=require('../model/post.model');
 
 
-async function createPostController(req, res) {
+// async function createPostController(req, res) {
 
- console.log(req.body,req.file );
- res.status(200)
-}
+//  console.log(req.body,req.file );
+//  res.status(200)
+// }
 
-module.exports={createPostController}
+// module.exports={createPostController}
+
+const Post = require('../model/post.model');
+
+const createPostController = async (req, res) => {
+  try {
+    const { caption } = req.body;
+
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        message: "Image file is required"
+      });
+    }
+
+    const newPost = await Post.create({
+      caption,
+      image: req.file.path,
+      user: req.user?.id
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Post created successfully",
+      data: newPost
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error: error.message
+    });
+  }
+};
+
+module.exports = { createPostController };
