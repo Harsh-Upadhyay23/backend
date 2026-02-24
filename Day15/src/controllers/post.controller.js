45b6492ee0c9cb5a581c86dc035263720f486eec
Harsh Upadhyay <upadhyayharsh435@gmail.com>
2026-02-24 22:14:33 +0530

    added Post

--- Day15/src/controllers/post.controller.js
+++ Day15/src/controllers/post.controller.js
@@ -1,19 +1,31 @@
 const postModel=require('../model/post.model')
 const ImageKit =require('@imagekit/nodejs')
 const {toFile}=require('@imagekit/nodejs');
+const { Folders } = require('@imagekit/nodejs/resources/index.js');
+const jwt=require('jsonwebtoken')
 
 
 const imagekit =  new ImageKit({
-    privateKey:process.env.IMAGEKIT_PRIIVATE_KEY
+    privateKey:process.env.IMAGEKIT_PRIVATE_KEY
 })
 
 
 async function createPostController(req, res) {
+    const token=req.cookies.token;
+    if(!token){
+        return res.status(401).json({
+            message:"token not provided,Unauthorized access"
+        })
+    }
 
- const file=await imagekit.files.upload({
+    const decoded=jwt.verify(token,process.env.JWT_SECRET)
+    console.log(decoded)
+    const file=await imagekit.files.upload({
     file:await toFile(Buffer.from(req.file.buffer),'file'),
     fileName:'Test',
+    folder:"insta-clone"
  })
+
   
 }
 
