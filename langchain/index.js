f128399d8a0dcdefbead7e005a2d61bd2ed5bcbd
Harsh Upadhyay <upadhyayharsh435@gmail.com>
2026-04-11 12:46:37 +0530

    added mistralai for chat

---
+++ langchain/index.js
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import readline from "readline";
+import { ChatMistralAI } from "@langchain/mistralai";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const model = new ChatMistralAI({
+  model: "mistral-small-latest",
+  temperature: 0,
+});
+
+async function chat() {
+  rl.question("You: ", async (input) => {
+    if (input.toLowerCase() === "exit") {
+      console.log("Bot: Goodbye 👋");
+      rl.close();
+      return;
+    }
+
+    try {
+      const response = await model.invoke(input);
+      // IMPORTANT: response.content use karo
+      console.log("Bot:", response.content);
+
+    } catch (error) {
+      console.log("Error:", error.message);
+    }
+
+    chat(); // loop again
+  });
+}
+
+chat();
